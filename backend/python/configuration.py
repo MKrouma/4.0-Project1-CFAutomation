ac349ea9b7cd8e5805b6c22ae77db234bcbf81aa
Mkrouma <mamadoukrouma@outlook.com>
2022-01-25 11:13:29 +0000

    master

--- backend/python/configuration.py
+++ backend/python/configuration.py
@@ -1,2 +1,2 @@
 PROJECT_PATH = "/Users/mac/Desktop/4.0-Project1-CFAutomation"
-MODULE_PATH = "/Users/mac/Desktop/4.0-Project1-CFAutomation/backend/module"
+MODULE_PATH = "/Users/mac/Desktop/4.0-Project1-CFAutomation/backend/python/module"
